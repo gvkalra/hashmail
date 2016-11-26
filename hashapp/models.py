7aebe2117b70a8aa7a4b47c27fdc97e3a26ff3fe
poguez <zeugop@gmail.com>
2016-11-26 13:38:59 +0900

    Tabs for indentation were substituted by spaces as PEP requires.

--- hashapp/models.py
+++ hashapp/models.py
@@ -1,28 +1,31 @@
 from django.db import models
-#from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-class HashTagModel(models.Model):
-	tag = models.CharField(max_length=30, unique=True)
 
-	def __str__(self):
-		return self.tag
+class HashTagModel(models.Model):
+    tag = models.CharField(max_length=30, unique=True)
+    
+    def __str__(self):
+        return self.tag
+
 
 class ImageModel(models.Model):
-	image = CloudinaryField('image')
-	tags = models.ManyToManyField(HashTagModel)
-	date = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField('image')
+    tags = models.ManyToManyField(HashTagModel)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        try:
+            public_id = self.image.public_id
+        except AttributeError:
+            public_id = ''
+        return "Photo <%s:%s>" % (self.title, public_id)
 
-	def __unicode__(self):
-		try:
-			public_id = self.image.public_id
-		except AttributeError:
-			public_id = ''
-		return "Photo <%s:%s>" % (self.title, public_id)
 
 """
 class UserProfile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	images = models.ManyToManyField(ImageModel)
-	tags = models.ManyToManyField(HashTagModel)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    images = models.ManyToManyField(ImageModel)
+    tags = models.ManyToManyField(HashTagModel)
 """
