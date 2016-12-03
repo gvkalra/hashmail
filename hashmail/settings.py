8d5919c284c14556c2bb3044f90068fd4a21c8e3
Gaurav Kalra <gvkalra@kaist.ac.kr>
2016-12-03 21:30:01 +0900

    adding celery with cloudamqp

--- hashmail/settings.py
+++ hashmail/settings.py
@@ -157,3 +157,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
 os.environ["CLOUDINARY_URL"]='cloudinary://193889446124356:w2zYrSQqsowK65p5Zvb-655j084@hyiclya8s'
+
+BROKER_URL = 'amqp://qnxnystc:xEz98ifaJTovW61lTEp30j8w-warcbG5@buck.rmq.cloudamqp.com/qnxnystc'
+BROKER_POOL_LIMIT = 1 # Will decrease connection usage
+BROKER_HEARTBEAT = None # We're using TCP keep-alive instead
+BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux DNS timeouts etc
+CELERY_RESULT_BACKEND = None # AMQP is not recommended as result backend as it creates thousands of queues
+CELERY_SEND_EVENTS = False # Will not create celeryev.* queues
+CELERY_EVENT_QUEUE_EXPIRES = 60 # Will delete all celeryev. queues without consumers after 1 minute.
